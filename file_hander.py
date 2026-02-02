eedecd074e12092b5c90bba7bbe6d0206abb11d4
unukalhai727 <chenxin.fang@hdu.edu.cn>
2026-02-02 23:32:36 +0800

    v1.0.1

--- file_hander.py
+++ file_hander.py
@@ -3,15 +3,16 @@ import io
 import re
 import zipfile
 
+import chardet
 from opencc import OpenCC
 
 
-XML_FILE_EXTENSIONS = {'opf', 'ncx', 'xml', 'xhtml', 'html', 'htm'}
+TXT_FILE_EXTENSIONS = {'opf', 'ncx', 'xml', 'xhtml', 'html', 'htm', 'txt'}
 
 converters = [OpenCC("t2tw.json"), OpenCC("tw2sp.json")]
-substitution_dict = {row[0]: row[1] for row in csv.reader(open('substitutions.csv', encoding='utf-8')) if len(row) >= 2}
 
-async def convert(text: str) -> str:
+
+async def convert(text: str, substitution_dict: dict[str, str]) -> str:
     for converter in converters:
         text = converter.convert(text)
     for pattern, repl in substitution_dict.items():
@@ -19,17 +20,23 @@ async def convert(text: str) -> str:
     return text
 
 async def processor(file: bytes, title: str) -> tuple[bytes, str]:
-    title = await convert(title)
+    substitution_dict = {row[0]: row[1] for row in csv.reader(open('substitutions.csv', encoding='utf-8')) if len(row) >= 2}
+    title = await convert(title, substitution_dict)
     output_buffer = io.BytesIO()
 
-    with zipfile.ZipFile(io.BytesIO(file), 'r') as zin, zipfile.ZipFile(output_buffer, 'w') as zout:
-        for info in zin.infolist():
-            if not info.filename.split('.')[-1].lower() in XML_FILE_EXTENSIONS:
-                zout.writestr(info.filename, zin.read(info.filename))
-                continue
+    try:
+        with zipfile.ZipFile(io.BytesIO(file), 'r') as zin, zipfile.ZipFile(output_buffer, 'w') as zout:
+            for info in zin.infolist():
+                if not info.filename.split('.')[-1].lower() in TXT_FILE_EXTENSIONS:
+                    zout.writestr(info.filename, zin.read(info.filename))
+                    continue
 
-            text = zin.read(info.filename).decode('utf-8')
-            text = await convert(text)
-            zout.writestr(info.filename, text.encode('utf-8'))
+                text = zin.read(info.filename).decode('utf-8')
+                text = await convert(text, substitution_dict)
+                zout.writestr(info.filename, text.encode('utf-8'))
+    except zipfile.BadZipFile:
+        text = file.decode(chardet.detect(file)['encoding'] or 'utf-8')
+        text = await convert(text, substitution_dict)
+        output_buffer.write(text.encode('utf-8'))
 
     return output_buffer.getvalue(), title
